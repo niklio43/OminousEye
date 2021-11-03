e979736ed37fd485f740381263916b2d9b017a4c
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-11-03 11:56:37 +0100

    added rikard stuff

---
+++ Ominous Eye/Assets/HealthBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    GameObject eye;
+    GameObject healthBarParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //healthBarParent = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //eye = GameObject.Find("Player");
+
+        //if(eye != null)
+        //{
+        //    healthBarParent.SetActive(false);
+        //}
+        //if(eye == null)
+        //{
+        //    healthBarParent.SetActive(true);
+        //}
+    }
+}

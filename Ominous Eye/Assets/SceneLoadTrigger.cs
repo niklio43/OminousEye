0fa38dd22c5eb3aaa005e814c88732f94eb77b0b
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2021-11-05 11:22:29 +0100

    101

--- Ominous Eye/Assets/SceneLoadTrigger.cs
+++ Ominous Eye/Assets/SceneLoadTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadTrigger : MonoBehaviour
 {
+    [SerializeField] private string restartString;
     [SerializeField] private string loadSceneString;
 
 
@@ -19,7 +20,7 @@ public class SceneLoadTrigger : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.R))
         {
-            SceneManager.LoadScene("Level1");
+            SceneManager.LoadScene(restartString);
         }
     }
 
